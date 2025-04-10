3f17dd1b03c4a8f93bc9260f12d18921593b43ad
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-10 10:35:45 +0200

    ADD milestone 3

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,46 @@ type Actress = Person & {
   awards: string;
   nationality: Nationality;
 };
+
+function isActress(data: unknown): data is Actress {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "id" in data &&
+    typeof data.id === "number" &&
+    "name" in data &&
+    typeof data.name === "string" &&
+    "birth_year" in data &&
+    typeof data.birth_year === "number" &&
+    // "death_year" in data &&
+    // typeof data.death_year === "number" &&
+    "biography" in data &&
+    typeof data.biography === "string" &&
+    "image" in data &&
+    typeof data.image === "string" &&
+    "most_famous_movies" in data &&
+    data.most_famous_movies instanceof Array &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every((movie) => typeof movie === "string") &&
+    "awards" in data &&
+    typeof data.awards === "string" &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`
+    );
+    const data: unknown = await response.json();
+    if (!isActress(data)) {
+      throw new Error("Formato dei dati non valido");
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
